a2587d5de4cb99bb3fcbf39adcd8df3675df1cb3
sridds <seth.riddensdale@mymail.champlain.edu>
2024-01-19 16:56:05 -0500

    Player Movement / Bounds
    Setup initial player movement script with basic movement and some bounds to keep the player inside

---
+++ SRiddensdale_VerticalShooter/Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 playerSpeed;
+
+    [SerializeField]
+    private float acceleration = 0.5f;
+    private float maxAccel = 50;
+
+    // internal variables
+    private Vector2 playerInput;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        // get references
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void GetInput()
+    {
+        // inputs
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // get input and normalize it, storing the result in the playerInput Vector2 variable
+        playerInput = new Vector2(x, y).normalized;
+    }
+
+    private void ApplyMovement()
+    {
+        Vector2 wishVel = playerInput * playerSpeed;
+
+        rb.velocity = wishVel;
+    }
+}
