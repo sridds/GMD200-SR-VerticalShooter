c664a24dac71e270d1790b467cc4e59ceeae04bf
sridds <seth.riddensdale@mymail.champlain.edu>
2024-01-21 13:26:09 -0500

    Began Spawner / Added Spawner Motion Parameters
    Spin / curve customization for ray casting

---
+++ SRiddensdale_VerticalShooter/Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Bullet Settings")]
+    [SerializeField]
+    private float _lifeTime;
+
+    void Update()
+    {
+        
+    }
+}
--- SRiddensdale_VerticalShooter/Assets/Spawner.cs
+++ SRiddensdale_VerticalShooter/Assets/Spawner.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Bullet Settings")]
+    [SerializeField]
+    private float _bulletForce;
+
+    [SerializeField]
+    private Bullet _bulletPrefab;
+
+    [Header("Fire Motion")]
+    [SerializeField]
+    private float _spinRate;
+
+    [SerializeField]
+    private bool _useCurve;
+
+    [SerializeField, Tooltip("Determines how much sin is added to the spin angle")]
+    [ShowIf(nameof(_useCurve))]
+    private float _curveSpeed;
+
+    [SerializeField]
+    [ShowIf(nameof(_useCurve))]
+    private float _curveAmplitude;
+
+    [SerializeField]
+    [ShowIf(nameof(_useCurve))]
+    private AnimationCurve _curve = new AnimationCurve(new Keyframe(0, -1), new Keyframe(1, 1));
+    Quaternion targetRot = Quaternion.identity;
+
+    private void Start()
     {
-        
+        // ensure curve is pingpong
+        _curve.postWrapMode = WrapMode.PingPong;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        targetRot *= Quaternion.Euler(0, 0, _spinRate * Time.deltaTime);
+
+        // apply curve rot
+        if(_useCurve) targetRot *= Quaternion.Euler(0, 0, (_curve.Evaluate(Time.time * _curveSpeed) * Time.deltaTime * _curveAmplitude));
+
+        Vector2 targetForward = targetRot * Vector2.up;
+        Debug.DrawRay(transform.position, targetForward * 15.0f, Color.blue);
+        Debug.DrawRay(transform.position, Vector2.up * 15.0f, Color.green);
     }
 }
