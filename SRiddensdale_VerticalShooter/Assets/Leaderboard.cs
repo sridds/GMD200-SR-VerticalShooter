7a440404e203ce78200221d52f89fa6f8765b334
sridds <seth.riddensdale@mymail.champlain.edu>
2024-01-30 14:03:02 -0500

    Leaderboard works
    At the moment, the leaderboard fetches and retrieves information from the server. Still need to add a way to record the score on the leaderboard, but it at least works. I'm gonna make the UI look nice once it's finished

--- SRiddensdale_VerticalShooter/Assets/Leaderboard.cs
+++ SRiddensdale_VerticalShooter/Assets/Leaderboard.cs
@@ -13,12 +13,42 @@ public class Leaderboard : MonoBehaviour
     private List<TextMeshProUGUI> _names;
     [SerializeField]
     private List<TextMeshProUGUI> _scores;
+    [SerializeField]
+    private List<TextMeshProUGUI> _ranks;
+    [SerializeField]
+    private GameObject _leaderboardHolder;
+    [SerializeField]
+    private GameObject _fetchingText;
+    [SerializeField]
+    private GameObject _namesHolder;
+    [SerializeField]
+    private GameObject _scoresHolder;
+    [SerializeField]
+    private GameObject _rankHolder;
 
     private const string PUBLIC_KEY = "55dc3e31e7c73aac7ce42cfef7149d8a7e99be5ba055d05afbfd1f9ce727a58c";
+
+    public void ShowLeaderboard()
+    {
+        _leaderboardHolder.SetActive(true);
+        _fetchingText.SetActive(true);
+
+        _scoresHolder.SetActive(false);
+        _namesHolder.SetActive(false);
+        _rankHolder.SetActive(false);
+
+        GetLeaderboard();
+    }
 
     public void GetLeaderboard()
     {
         LeaderboardCreator.GetLeaderboard(PUBLIC_KEY, (msg) => {
+            _fetchingText.SetActive(false);
+
+            _scoresHolder.SetActive(true);
+            _namesHolder.SetActive(true);
+            _rankHolder.SetActive(true);
+
             HandleLeaderboard(msg);
         });
     }
@@ -27,8 +57,18 @@ public class Leaderboard : MonoBehaviour
     {
         for(int i = 0; i < _names.Count; i++)
         {
-            _names[i].text = msg[i].Username;
-            _scores[i].text = msg[i].Score.ToString();
+            if(i < msg.Length)
+            {
+                _names[i].text = msg[i].Username;
+                _scores[i].text = $"{msg[i].Score:D7}";
+                
+            }
+            else {
+                _names[i].text = "------";
+                _scores[i].text = "-------";
+            }
+
+            _ranks[i].text = $"#{i + 1}";
         }
     }
 
--- SRiddensdale_VerticalShooter/Assets/Scripts/StartMenu.cs
+++ SRiddensdale_VerticalShooter/Assets/Scripts/StartMenu.cs
@@ -8,7 +8,11 @@ public class StartMenu : MonoBehaviour
     [SerializeField]
     private float _waitTime = 0.5f;
     [SerializeField]
-    private Animator _textAnimation;
+    private Animator _startTextAnimation;
+    [SerializeField]
+    private Animator _leaderboardTextAnimation;
+    [SerializeField]
+    private RectTransform _shipSelector;
     [SerializeField]
     private Animator _fade;
     [SerializeField]
@@ -18,9 +22,14 @@ public class StartMenu : MonoBehaviour
     [SerializeField]
     private AudioData _selectSound;
     [SerializeField]
+    private AudioData _menuSelectSound;
+    [SerializeField]
     private GameObject _whiteFlash;
+    [SerializeField]
+    private Leaderboard _leaderboard;
 
     private bool canSelect = false;
+    int selection = 0;
 
     private void Start()
     {
@@ -33,17 +42,58 @@ public class StartMenu : MonoBehaviour
     {
         _menuElements.SetActive(true);
         canSelect = true;
+
+        _startTextAnimation.SetBool("OnText", true);
+        _leaderboardTextAnimation.SetBool("OnText", false);
     }
 
     private void Update()
     {
+        int newSelection = selection;
+
         if (canSelect) {
-            if (Input.anyKeyDown) {
-                StartCoroutine(StartGame());
-                canSelect = false;
-                _whiteFlash.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                if(selection == 0)
+                {
+                    StartCoroutine(StartGame());
+                    canSelect = false;
+                    _whiteFlash.SetActive(true);
+                }
+                if(selection == 1)
+                {
+                    _leaderboard.ShowLeaderboard();
+                    canSelect = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                newSelection = 1;
+
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                newSelection = 0;
             }
         }
+
+
+        if(selection != newSelection) {
+            AudioHandler.instance.ProcessAudioData(_menuSelectSound);
+
+            if(newSelection == 0) {
+                _startTextAnimation.SetBool("OnText", true);
+                _leaderboardTextAnimation.SetBool("OnText", false);
+                _shipSelector.anchoredPosition = new Vector2(_shipSelector.anchoredPosition.x, _shipSelector.anchoredPosition.y + 64);
+
+            }
+            else {
+                _startTextAnimation.SetBool("OnText", false);
+                _leaderboardTextAnimation.SetBool("OnText", true);
+                _shipSelector.anchoredPosition = new Vector2(_shipSelector.anchoredPosition.x, _shipSelector.anchoredPosition.y - 64);
+            }
+        }
+        selection = newSelection;
     }
 
     private IEnumerator StartGame()
@@ -53,7 +103,7 @@ public class StartMenu : MonoBehaviour
         Time.timeScale = 0.0f;
         AudioHandler.instance.ProcessAudioData(_selectSound);
 
-        _textAnimation.SetBool("Select", true);
+        _startTextAnimation.SetBool("Select", true);
         yield return new WaitForSecondsRealtime(_menuHoldTime);
         // fade out 
         _fade.SetTrigger("FadeOut");
